b8b0c25c1ed2168319f3fe6ec77459865558d6af
Billy Keyes <bluekeyes@gmail.com>
2019-03-19 21:59:30 -0700

    Move file header functions to a new file

---
+++ gitdiff/file_header.go
@@ -0,0 +1,266 @@
+package gitdiff
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// parseGitHeaderName extracts a default file name from the Git file header
+// line. This is required for mode-only changes and creation/deletion of empty
+// files. Other types of patch include the file name(s) in the header data.
+// If the names in the header do not match because the patch is a rename,
+// return an empty default name.
+func parseGitHeaderName(header string) (string, error) {
+	firstName, n, err := parseName(header, -1, 1)
+	if err != nil {
+		return "", err
+	}
+
+	if n < len(header) && (header[n] == ' ' || header[n] == '\t') {
+		n++
+	}
+
+	secondName, _, err := parseName(header[n:], -1, 1)
+	if err != nil {
+		return "", err
+	}
+
+	if firstName != secondName {
+		return "", nil
+	}
+	return firstName, nil
+}
+
+// parseGitHeaderData parses a single line of metadata from a Git file header.
+// It returns true when header parsing is complete; in that case, line was the
+// first line of non-header content.
+func parseGitHeaderData(f *File, line, defaultName string) (end bool, err error) {
+	if line[len(line)-1] == '\n' {
+		line = line[:len(line)-1]
+	}
+
+	for _, hdr := range []struct {
+		prefix string
+		end    bool
+		parse  func(*File, string, string) error
+	}{
+		{fragmentHeaderPrefix, true, nil},
+		{oldFilePrefix, false, parseGitHeaderOldName},
+		{newFilePrefix, false, parseGitHeaderNewName},
+		{"old mode ", false, parseGitHeaderOldMode},
+		{"new mode ", false, parseGitHeaderNewMode},
+		{"deleted file mode ", false, parseGitHeaderDeletedMode},
+		{"new file mode ", false, parseGitHeaderCreatedMode},
+		{"copy from ", false, parseGitHeaderCopyFrom},
+		{"copy to ", false, parseGitHeaderCopyTo},
+		{"rename old ", false, parseGitHeaderRenameFrom},
+		{"rename new ", false, parseGitHeaderRenameTo},
+		{"rename from ", false, parseGitHeaderRenameFrom},
+		{"rename to ", false, parseGitHeaderRenameTo},
+		{"similarity index ", false, parseGitHeaderScore},
+		{"dissimilarity index ", false, parseGitHeaderScore},
+		{"index ", false, parseGitHeaderIndex},
+	} {
+		if strings.HasPrefix(line, hdr.prefix) {
+			if hdr.parse != nil {
+				err = hdr.parse(f, line[len(hdr.prefix):], defaultName)
+			}
+			return hdr.end, err
+		}
+	}
+
+	// unknown line indicates the end of the header
+	// this usually happens if the diff is empty
+	return true, nil
+}
+
+func parseGitHeaderOldName(f *File, line, defaultName string) error {
+	name, _, err := parseName(line, '\t', 1)
+	if err != nil {
+		return err
+	}
+	if f.OldName == "" && !f.IsNew {
+		f.OldName = name
+		return nil
+	}
+	return verifyGitHeaderName(name, f.OldName, f.IsNew, "old")
+}
+
+func parseGitHeaderNewName(f *File, line, defaultName string) error {
+	name, _, err := parseName(line, '\t', 1)
+	if err != nil {
+		return err
+	}
+	if f.NewName == "" && !f.IsDelete {
+		f.NewName = name
+		return nil
+	}
+	return verifyGitHeaderName(name, f.NewName, f.IsDelete, "new")
+}
+
+func parseGitHeaderOldMode(f *File, line, defaultName string) (err error) {
+	f.OldMode, err = parseMode(line)
+	return
+}
+
+func parseGitHeaderNewMode(f *File, line, defaultName string) (err error) {
+	f.NewMode, err = parseMode(line)
+	return
+}
+
+func parseGitHeaderDeletedMode(f *File, line, defaultName string) error {
+	f.IsDelete = true
+	f.OldName = defaultName
+	return parseGitHeaderOldMode(f, line, defaultName)
+}
+
+func parseGitHeaderCreatedMode(f *File, line, defaultName string) error {
+	f.IsNew = true
+	f.NewName = defaultName
+	return parseGitHeaderNewMode(f, line, defaultName)
+}
+
+func parseGitHeaderCopyFrom(f *File, line, defaultName string) (err error) {
+	f.IsCopy = true
+	f.OldName, _, err = parseName(line, -1, 0)
+	return
+}
+
+func parseGitHeaderCopyTo(f *File, line, defaultName string) (err error) {
+	f.IsCopy = true
+	f.NewName, _, err = parseName(line, -1, 0)
+	return
+}
+
+func parseGitHeaderRenameFrom(f *File, line, defaultName string) (err error) {
+	f.IsRename = true
+	f.OldName, _, err = parseName(line, -1, 0)
+	return
+}
+
+func parseGitHeaderRenameTo(f *File, line, defaultName string) (err error) {
+	f.IsRename = true
+	f.NewName, _, err = parseName(line, -1, 0)
+	return
+}
+
+func parseGitHeaderScore(f *File, line, defaultName string) error {
+	score, err := strconv.ParseInt(line, 10, 32)
+	if err != nil {
+		nerr := err.(*strconv.NumError)
+		return fmt.Errorf("invalid score line: %v", nerr.Err)
+	}
+	if score <= 100 {
+		f.Score = int(score)
+	}
+	return nil
+}
+
+func parseGitHeaderIndex(f *File, line, defaultName string) error {
+	const sep = ".."
+
+	parts := strings.SplitN(line, " ", 2)
+	oids := strings.SplitN(parts[0], sep, 2)
+
+	if len(oids) < 2 {
+		return fmt.Errorf("invalid index line: missing %q", sep)
+	}
+	f.OldOID, f.NewOID = oids[0], oids[1]
+
+	if len(parts) > 1 {
+		return parseGitHeaderOldMode(f, parts[1], defaultName)
+	}
+	return nil
+}
+
+func parseMode(s string) (os.FileMode, error) {
+	mode, err := strconv.ParseInt(s, 8, 32)
+	if err != nil {
+		nerr := err.(*strconv.NumError)
+		return os.FileMode(0), fmt.Errorf("invalid mode line: %v", nerr.Err)
+	}
+	return os.FileMode(mode), nil
+}
+
+// parseName extracts a file name from the start of a string and returns the
+// name and the index of the first character after the name. If the name is
+// unquoted and term is non-negative, parsing stops at the first occurance of
+// term. Otherwise parsing of unquoted names stops at the first space or tab.
+//
+// If the name is exactly "/dev/null", no further processing occurs. Otherwise,
+// if dropPrefix is greater than zero, that number of prefix components
+// separated by forward slashes are dropped from the name and any duplicate
+// slashes are collapsed.
+func parseName(s string, term rune, dropPrefix int) (name string, n int, err error) {
+	if len(s) > 0 && s[0] == '"' {
+		// find matching end quote and then unquote the section
+		for n = 1; n < len(s); n++ {
+			if s[n] == '"' && s[n-1] != '\\' {
+				n++
+				break
+			}
+		}
+		if n == 2 {
+			return "", 0, fmt.Errorf("missing name")
+		}
+		if name, err = strconv.Unquote(s[:n]); err != nil {
+			return "", 0, err
+		}
+	} else {
+		// find terminator and take the previous section
+		for n = 0; n < len(s); n++ {
+			if term >= 0 && rune(s[n]) == term {
+				break
+			}
+			if term < 0 && (s[n] == ' ' || s[n] == '\t') {
+				break
+			}
+		}
+		if n == 0 {
+			return "", 0, fmt.Errorf("missing name")
+		}
+		name = s[:n]
+	}
+
+	if name == devNull {
+		return name, n, nil
+	}
+	return cleanName(name, dropPrefix), n, nil
+}
+
+// verifyGitHeaderName checks a parsed name against state set by previous lines
+func verifyGitHeaderName(parsed, existing string, isNull bool, side string) error {
+	if existing != "" {
+		if isNull {
+			return fmt.Errorf("expected %s, but filename is set to %s", devNull, existing)
+		}
+		if existing != parsed {
+			return fmt.Errorf("inconsistent %s filename", side)
+		}
+	}
+	if isNull && parsed != devNull {
+		return fmt.Errorf("expected %s", devNull)
+	}
+	return nil
+}
+
+// cleanName removes double slashes and drops prefix segments.
+func cleanName(name string, drop int) string {
+	var b strings.Builder
+	for i := 0; i < len(name); i++ {
+		if name[i] == '/' {
+			if i < len(name)-1 && name[i+1] == '/' {
+				continue
+			}
+			if drop > 0 {
+				drop--
+				b.Reset()
+				continue
+			}
+		}
+		b.WriteByte(name[i])
+	}
+	return b.String()
+}
---
+++ gitdiff/file_header_test.go
@@ -0,0 +1,390 @@
+package gitdiff
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestCleanName(t *testing.T) {
+	tests := map[string]struct {
+		Input  string
+		Drop   int
+		Output string
+	}{
+		"alreadyClean": {
+			Input: "a/b/c.txt", Output: "a/b/c.txt",
+		},
+		"doubleSlashes": {
+			Input: "a//b/c.txt", Output: "a/b/c.txt",
+		},
+		"tripleSlashes": {
+			Input: "a///b/c.txt", Output: "a/b/c.txt",
+		},
+		"dropPrefix": {
+			Input: "a/b/c.txt", Drop: 2, Output: "c.txt",
+		},
+		"removeDoublesBeforeDrop": {
+			Input: "a//b/c.txt", Drop: 1, Output: "b/c.txt",
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			output := cleanName(test.Input, test.Drop)
+			if output != test.Output {
+				t.Fatalf("incorrect output: expected %q, actual %q", test.Output, output)
+			}
+		})
+	}
+}
+
+func TestParseName(t *testing.T) {
+	tests := map[string]struct {
+		Input  string
+		Term   rune
+		Drop   int
+		Output string
+		N      int
+		Err    bool
+	}{
+		"singleUnquoted": {
+			Input: "dir/file.txt", Output: "dir/file.txt", N: 12,
+		},
+		"singleQuoted": {
+			Input: `"dir/file.txt"`, Output: "dir/file.txt", N: 14,
+		},
+		"quotedWithEscape": {
+			Input: `"dir/\"quotes\".txt"`, Output: `dir/"quotes".txt`, N: 20,
+		},
+		"quotedWithSpaces": {
+			Input: `"dir/space file.txt"`, Output: "dir/space file.txt", N: 20,
+		},
+		"tabTerminator": {
+			Input: "dir/space file.txt\tfile2.txt", Term: '\t', Output: "dir/space file.txt", N: 18,
+		},
+		"dropPrefix": {
+			Input: "a/dir/file.txt", Drop: 1, Output: "dir/file.txt", N: 14,
+		},
+		"multipleNames": {
+			Input: "dir/a.txt dir/b.txt", Term: -1, Output: "dir/a.txt", N: 9,
+		},
+		"devNull": {
+			Input: "/dev/null", Term: '\t', Drop: 1, Output: "/dev/null", N: 9,
+		},
+		"emptyString": {
+			Input: "", Err: true,
+		},
+		"emptyQuotedString": {
+			Input: `""`, Err: true,
+		},
+		"unterminatedQuotes": {
+			Input: `"dir/file.txt`, Err: true,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			output, n, err := parseName(test.Input, test.Term, test.Drop)
+			if test.Err {
+				if err == nil {
+					t.Fatalf("expected error parsing name, but got nil")
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("unexpected error parsing name: %v", err)
+			}
+
+			if output != test.Output {
+				t.Errorf("incorect output: expected %q, actual: %q", test.Output, output)
+			}
+			if n != test.N {
+				t.Errorf("incorrect next position: expected %d, actual %d", test.N, n)
+			}
+		})
+	}
+}
+
+func TestParseGitHeaderData(t *testing.T) {
+	tests := map[string]struct {
+		InputFile   *File
+		Line        string
+		DefaultName string
+
+		OutputFile *File
+		End        bool
+		Err        bool
+	}{
+		"fragementEndsParsing": {
+			Line: "@@ -12,3 +12,2 @@\n",
+			End:  true,
+		},
+		"unknownEndsParsing": {
+			Line: "GIT binary file\n",
+			End:  true,
+		},
+		"oldFileName": {
+			Line: "--- a/dir/file.txt\n",
+			OutputFile: &File{
+				OldName: "dir/file.txt",
+			},
+		},
+		"oldFileNameDevNull": {
+			InputFile: &File{
+				IsNew: true,
+			},
+			Line: "--- /dev/null\n",
+			OutputFile: &File{
+				IsNew: true,
+			},
+		},
+		"oldFileNameInconsistent": {
+			InputFile: &File{
+				OldName: "dir/foo.txt",
+			},
+			Line: "--- a/dir/bar.txt\n",
+			Err:  true,
+		},
+		"oldFileNameExistingCreateMismatch": {
+			InputFile: &File{
+				OldName: "dir/foo.txt",
+				IsNew:   true,
+			},
+			Line: "--- /dev/null\n",
+			Err:  true,
+		},
+		"oldFileNameParsedCreateMismatch": {
+			InputFile: &File{
+				IsNew: true,
+			},
+			Line: "--- a/dir/file.txt\n",
+			Err:  true,
+		},
+		"oldFileNameMissing": {
+			Line: "--- \n",
+			Err:  true,
+		},
+		"newFileName": {
+			Line: "+++ b/dir/file.txt\n",
+			OutputFile: &File{
+				NewName: "dir/file.txt",
+			},
+		},
+		"newFileNameDevNull": {
+			InputFile: &File{
+				IsDelete: true,
+			},
+			Line: "+++ /dev/null\n",
+			OutputFile: &File{
+				IsDelete: true,
+			},
+		},
+		"newFileNameInconsistent": {
+			InputFile: &File{
+				NewName: "dir/foo.txt",
+			},
+			Line: "+++ b/dir/bar.txt\n",
+			Err:  true,
+		},
+		"newFileNameExistingDeleteMismatch": {
+			InputFile: &File{
+				NewName:  "dir/foo.txt",
+				IsDelete: true,
+			},
+			Line: "+++ /dev/null\n",
+			Err:  true,
+		},
+		"newFileNameParsedDeleteMismatch": {
+			InputFile: &File{
+				IsDelete: true,
+			},
+			Line: "+++ b/dir/file.txt\n",
+			Err:  true,
+		},
+		"newFileNameMissing": {
+			Line: "+++ \n",
+			Err:  true,
+		},
+		"oldMode": {
+			Line: "old mode 100644\n",
+			OutputFile: &File{
+				OldMode: os.FileMode(0100644),
+			},
+		},
+		"invalidOldMode": {
+			Line: "old mode rw\n",
+			Err:  true,
+		},
+		"newMode": {
+			Line: "new mode 100755\n",
+			OutputFile: &File{
+				NewMode: os.FileMode(0100755),
+			},
+		},
+		"invalidNewMode": {
+			Line: "new mode rwx\n",
+			Err:  true,
+		},
+		"deletedFileMode": {
+			Line:        "deleted file mode 100644\n",
+			DefaultName: "dir/file.txt",
+			OutputFile: &File{
+				OldName:  "dir/file.txt",
+				OldMode:  os.FileMode(0100644),
+				IsDelete: true,
+			},
+		},
+		"newFileMode": {
+			Line:        "new file mode 100755\n",
+			DefaultName: "dir/file.txt",
+			OutputFile: &File{
+				NewName: "dir/file.txt",
+				NewMode: os.FileMode(0100755),
+				IsNew:   true,
+			},
+		},
+		"copyFrom": {
+			Line: "copy from dir/file.txt\n",
+			OutputFile: &File{
+				OldName: "dir/file.txt",
+				IsCopy:  true,
+			},
+		},
+		"copyTo": {
+			Line: "copy to dir/file.txt\n",
+			OutputFile: &File{
+				NewName: "dir/file.txt",
+				IsCopy:  true,
+			},
+		},
+		"renameFrom": {
+			Line: "rename from dir/file.txt\n",
+			OutputFile: &File{
+				OldName:  "dir/file.txt",
+				IsRename: true,
+			},
+		},
+		"renameTo": {
+			Line: "rename to dir/file.txt\n",
+			OutputFile: &File{
+				NewName:  "dir/file.txt",
+				IsRename: true,
+			},
+		},
+		"similarityIndex": {
+			Line: "similarity index 88\n",
+			OutputFile: &File{
+				Score: 88,
+			},
+		},
+		"similarityIndexTooBig": {
+			Line: "similarity index 9001\n",
+			OutputFile: &File{
+				Score: 0,
+			},
+		},
+		"similarityIndexInvalid": {
+			Line: "similarity index 12ab\n",
+			Err:  true,
+		},
+		"indexFullSHA1AndMode": {
+			Line: "index 79c6d7f7b7e76c75b3d238f12fb1323f2333ba14..04fab916d8f938173cbb8b93469855f0e838f098 100644\n",
+			OutputFile: &File{
+				OldOID:  "79c6d7f7b7e76c75b3d238f12fb1323f2333ba14",
+				NewOID:  "04fab916d8f938173cbb8b93469855f0e838f098",
+				OldMode: os.FileMode(0100644),
+			},
+		},
+		"indexFullSHA1NoMode": {
+			Line: "index 79c6d7f7b7e76c75b3d238f12fb1323f2333ba14..04fab916d8f938173cbb8b93469855f0e838f098\n",
+			OutputFile: &File{
+				OldOID: "79c6d7f7b7e76c75b3d238f12fb1323f2333ba14",
+				NewOID: "04fab916d8f938173cbb8b93469855f0e838f098",
+			},
+		},
+		"indexAbbrevSHA1AndMode": {
+			Line: "index 79c6d7..04fab9 100644\n",
+			OutputFile: &File{
+				OldOID:  "79c6d7",
+				NewOID:  "04fab9",
+				OldMode: os.FileMode(0100644),
+			},
+		},
+		"indexInvalid": {
+			Line: "index 79c6d7f7b7e76c75b3d238f12fb1323f2333ba14\n",
+			Err:  true,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			var f File
+			if test.InputFile != nil {
+				f = *test.InputFile
+			}
+
+			end, err := parseGitHeaderData(&f, test.Line, test.DefaultName)
+			if test.Err {
+				if err == nil {
+					t.Fatalf("expected error parsing header data, but got nil")
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("unexpected error parsing header data: %v", err)
+			}
+
+			if test.OutputFile != nil && !reflect.DeepEqual(test.OutputFile, &f) {
+				t.Errorf("incorrect output:\nexpected: %+v\nactual: %+v", test.OutputFile, &f)
+			}
+			if end != test.End {
+				t.Errorf("incorrect end state, expected %t, actual %t", test.End, end)
+			}
+		})
+	}
+}
+
+func TestParseGitHeaderName(t *testing.T) {
+	tests := map[string]struct {
+		Input  string
+		Output string
+		Err    bool
+	}{
+		"twoMatchingNames": {
+			Input:  "a/dir/file.txt b/dir/file.txt",
+			Output: "dir/file.txt",
+		},
+		"twoDifferentNames": {
+			Input:  "a/dir/foo.txt b/dir/bar.txt",
+			Output: "",
+		},
+		"missingSecondName": {
+			Input: "a/dir/foo.txt",
+			Err:   true,
+		},
+		"invalidName": {
+			Input: `"a/dir/file.txt b/dir/file.txt`,
+			Err:   true,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			output, err := parseGitHeaderName(test.Input)
+			if test.Err {
+				if err == nil {
+					t.Fatalf("expected error parsing header name, but got nil")
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("unexpected error parsing header name: %v", err)
+			}
+
+			if output != test.Output {
+				t.Errorf("incorrect output: expected %q, actual %q", test.Output, output)
+			}
+		})
+	}
+}
