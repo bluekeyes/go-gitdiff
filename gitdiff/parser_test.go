631c70505ae6fcbd17b60a644f18ce521f711f8b
Billy Keyes <bluekeyes@gmail.com>
2019-03-19 23:05:15 -0700

    Add basic tests for full git file header parsing

--- gitdiff/parser_test.go
+++ gitdiff/parser_test.go
@@ -2,6 +2,7 @@ package gitdiff
 
 import (
 	"bufio"
+	"os"
 	"reflect"
 	"strings"
 	"testing"
@@ -124,6 +125,64 @@ func TestParseFragmentHeader(t *testing.T) {
 
 			if !reflect.DeepEqual(*test.Output, frag) {
 				t.Fatalf("incorrect fragment\nexpected: %+v\nactual: %+v", *test.Output, frag)
+			}
+		})
+	}
+}
+
+func TestParseGitFileHeader(t *testing.T) {
+	tests := map[string]struct {
+		Input  string
+		Output *File
+		Err    bool
+	}{
+		"simpleFileChange": {
+			Input: `diff --git a/dir/file.txt b/dir/file.txt
+index 1c23fcc..40a1b33 100644
+--- a/dir/file.txt
++++ b/dir/file.txt
+`,
+			Output: &File{
+				OldName:      "dir/file.txt",
+				NewName:      "dir/file.txt",
+				OldMode:      os.FileMode(0100644),
+				OldOIDPrefix: "1c23fcc",
+				NewOIDPrefix: "40a1b33",
+			},
+		},
+		"fileCreation": {
+			Input: `diff --git a/dir/file.txt b/dir/file.txt
+new file mode 100644
+index 0000000..f5711e4
+--- /dev/null
++++ b/dir/file.txt
+`,
+			Output: &File{
+				NewName:      "dir/file.txt",
+				NewMode:      os.FileMode(0100644),
+				OldOIDPrefix: "0000000",
+				NewOIDPrefix: "f5711e4",
+				IsNew:        true,
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			p := &parser{r: bufio.NewReader(strings.NewReader(test.Input))}
+			header, _ := p.Line()
+
+			var f File
+			err := p.ParseGitFileHeader(&f, header)
+			if test.Err {
+				if err == nil {
+					t.Fatalf("expected error parsing git header, got nil")
+				}
+				return
+			}
+
+			if test.Output != nil && !reflect.DeepEqual(f, *test.Output) {
+				t.Errorf("incorrect file\nexpected: %+v\nactual: %+v", *test.Output, f)
 			}
 		})
 	}
