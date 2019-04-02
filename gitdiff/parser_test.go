d32b8895351d5dc3c49963ed89f317779d218b57
Billy Keyes <bluekeyes@gmail.com>
2019-04-01 21:46:36 -0700

    Move text parsing tests to a new test files
    These are about to get even longer, so it makes sense to separate them.
    Also refactor the remaining parser tests to remove some duplication.

--- gitdiff/parser_test.go
+++ gitdiff/parser_test.go
@@ -3,7 +3,6 @@ package gitdiff
 import (
 	"bufio"
 	"io"
-	"reflect"
 	"strings"
 	"testing"
 )
@@ -13,45 +12,28 @@ func TestLineOperations(t *testing.T) {
 
 	t.Run("read", func(t *testing.T) {
 		p := newTestParser(content, false)
-		if err := p.Next(); err != nil {
-			t.Fatalf("error advancing parser: %v", err)
-		}
-		if p.lineno != 1 {
-			t.Fatalf("incorrect line number: expected %d, actual: %d", 1, p.lineno)
-		}
 
-		line := p.Line(0)
-		if line != "the first line\n" {
-			t.Fatalf("incorrect first line: %s", line)
-		}
+		for i, expected := range []string{
+			"the first line\n",
+			"the second line\n",
+			"the third line\n",
+		} {
+			if err := p.Next(); err != nil {
+				t.Fatalf("error advancing parser after line %d: %v", i, err)
+			}
+			if p.lineno != int64(i+1) {
+				t.Fatalf("incorrect line number: expected %d, actual: %d", i+1, p.lineno)
+			}
 
-		if err := p.Next(); err != nil {
-			t.Fatalf("error advancing parser: %v", err)
-		}
-		if p.lineno != 2 {
-			t.Fatalf("incorrect line number: expected %d, actual: %d", 2, p.lineno)
-		}
-
-		line = p.Line(0)
-		if line != "the second line\n" {
-			t.Fatalf("incorrect second line: %s", line)
-		}
-
-		if err := p.Next(); err != nil {
-			t.Fatalf("error advancing parser: %v", err)
-		}
-		if p.lineno != 3 {
-			t.Fatalf("incorrect line number: expected %d, actual: %d", 3, p.lineno)
-		}
-
-		line = p.Line(0)
-		if line != "the third line\n" {
-			t.Fatalf("incorrect third line: %s", line)
+			line := p.Line(0)
+			if line != expected {
+				t.Fatalf("incorrect line %d: expected %q, was %q", i+1, expected, line)
+			}
 		}
 
 		// reading after the last line should return EOF
 		if err := p.Next(); err != io.EOF {
-			t.Fatalf("expected EOF, but got: %v", err)
+			t.Fatalf("expected EOF after end, but got: %v", err)
 		}
 		if p.lineno != 4 {
 			t.Fatalf("incorrect line number: expected %d, actual: %d", 4, p.lineno)
@@ -59,7 +41,7 @@ func TestLineOperations(t *testing.T) {
 
 		// reading again returns EOF again and does not advance the line
 		if err := p.Next(); err != io.EOF {
-			t.Fatalf("expected EOF, but got: %v", err)
+			t.Fatalf("expected EOF after end, but got: %v", err)
 		}
 		if p.lineno != 4 {
 			t.Fatalf("incorrect line number: expected %d, actual: %d", 4, p.lineno)
@@ -78,19 +60,19 @@ func TestLineOperations(t *testing.T) {
 		}
 
 		if err := p.Next(); err != nil {
-			t.Fatalf("error advancing parser: %v", err)
+			t.Fatalf("error advancing parser after peek: %v", err)
 		}
 
 		line = p.Line(0)
 		if line != "the second line\n" {
-			t.Fatalf("incorrect line: %s", line)
+			t.Fatalf("incorrect read line: %s", line)
 		}
 	})
 
 	t.Run("emptyInput", func(t *testing.T) {
 		p := newTestParser("", false)
 		if err := p.Next(); err != io.EOF {
-			t.Fatalf("expected EOF, but got: %v", err)
+			t.Fatalf("expected EOF on first Next(), but got: %v", err)
 		}
 	})
 }
@@ -149,315 +131,6 @@ context line
 
 			if test.EndLine != p.Line(0) {
 				t.Errorf("incorrect position after parsing\nexpected: %q\nactual: %q", test.EndLine, p.Line(0))
-			}
-		})
-	}
-}
-
-func TestParseTextFragmentHeader(t *testing.T) {
-	tests := map[string]struct {
-		Input  string
-		Output *Fragment
-		Err    bool
-	}{
-		"shortest": {
-			Input: "@@ -1 +1 @@\n",
-			Output: &Fragment{
-				OldPosition: 1,
-				OldLines:    1,
-				NewPosition: 1,
-				NewLines:    1,
-			},
-		},
-		"standard": {
-			Input: "@@ -21,5 +28,9 @@\n",
-			Output: &Fragment{
-				OldPosition: 21,
-				OldLines:    5,
-				NewPosition: 28,
-				NewLines:    9,
-			},
-		},
-		"trailingComment": {
-			Input: "@@ -21,5 +28,9 @@ func test(n int) {\n",
-			Output: &Fragment{
-				Comment:     "func test(n int) {",
-				OldPosition: 21,
-				OldLines:    5,
-				NewPosition: 28,
-				NewLines:    9,
-			},
-		},
-		"incomplete": {
-			Input: "@@ -12,3 +2\n",
-			Err:   true,
-		},
-		"badNumbers": {
-			Input: "@@ -1a,2b +3c,4d @@\n",
-			Err:   true,
-		},
-	}
-
-	for name, test := range tests {
-		t.Run(name, func(t *testing.T) {
-			p := newTestParser(test.Input, true)
-
-			frag, err := p.ParseTextFragmentHeader()
-			if test.Err {
-				if err == nil {
-					t.Fatalf("expected error parsing header, but got nil")
-				}
-				return
-			}
-			if err != nil {
-				t.Fatalf("error parsing header: %v", err)
-			}
-
-			if !reflect.DeepEqual(test.Output, frag) {
-				t.Errorf("incorrect fragment\nexpected: %+v\nactual: %+v", test.Output, frag)
-			}
-		})
-	}
-}
-
-func TestParseTextChunk(t *testing.T) {
-	tests := map[string]struct {
-		Input    string
-		Fragment Fragment
-
-		Output *Fragment
-		Err    bool
-	}{
-		"addWithContext": {
-			Input: ` context line
-+new line 1
-+new line 2
- context line
-`,
-			Fragment: Fragment{
-				OldLines: 2,
-				NewLines: 4,
-			},
-			Output: &Fragment{
-				OldLines: 2,
-				NewLines: 4,
-				Lines: []FragmentLine{
-					{OpContext, "context line\n"},
-					{OpAdd, "new line 1\n"},
-					{OpAdd, "new line 2\n"},
-					{OpContext, "context line\n"},
-				},
-				LinesAdded:      2,
-				LeadingContext:  1,
-				TrailingContext: 1,
-			},
-		},
-		"deleteWithContext": {
-			Input: ` context line
--old line 1
--old line 2
- context line
-`,
-			Fragment: Fragment{
-				OldLines: 4,
-				NewLines: 2,
-			},
-			Output: &Fragment{
-				OldLines: 4,
-				NewLines: 2,
-				Lines: []FragmentLine{
-					{OpContext, "context line\n"},
-					{OpDelete, "old line 1\n"},
-					{OpDelete, "old line 2\n"},
-					{OpContext, "context line\n"},
-				},
-				LinesDeleted:    2,
-				LeadingContext:  1,
-				TrailingContext: 1,
-			},
-		},
-		"replaceWithContext": {
-			Input: ` context line
--old line 1
-+new line 1
- context line
-`,
-			Fragment: Fragment{
-				OldLines: 3,
-				NewLines: 3,
-			},
-			Output: &Fragment{
-				OldLines: 3,
-				NewLines: 3,
-				Lines: []FragmentLine{
-					{OpContext, "context line\n"},
-					{OpDelete, "old line 1\n"},
-					{OpAdd, "new line 1\n"},
-					{OpContext, "context line\n"},
-				},
-				LinesDeleted:    1,
-				LinesAdded:      1,
-				LeadingContext:  1,
-				TrailingContext: 1,
-			},
-		},
-		"deleteFinalNewline": {
-			Input: ` context line
--old line 1
-+new line 1
-\ No newline at end of file
-`,
-			Fragment: Fragment{
-				OldLines: 2,
-				NewLines: 2,
-			},
-			Output: &Fragment{
-				OldLines: 2,
-				NewLines: 2,
-				Lines: []FragmentLine{
-					{OpContext, "context line\n"},
-					{OpDelete, "old line 1\n"},
-					{OpAdd, "new line 1"},
-				},
-				LinesDeleted:   1,
-				LinesAdded:     1,
-				LeadingContext: 1,
-			},
-		},
-		"addFinalNewline": {
-			Input: ` context line
--old line 1
-\ No newline at end of file
-+new line 1
-`,
-			Fragment: Fragment{
-				OldLines: 2,
-				NewLines: 2,
-			},
-			Output: &Fragment{
-				OldLines: 2,
-				NewLines: 2,
-				Lines: []FragmentLine{
-					{OpContext, "context line\n"},
-					{OpDelete, "old line 1"},
-					{OpAdd, "new line 1\n"},
-				},
-				LinesDeleted:   1,
-				LinesAdded:     1,
-				LeadingContext: 1,
-			},
-		},
-		"addAll": {
-			Input: `+new line 1
-+new line 2
-+new line 3
-`,
-			Fragment: Fragment{
-				OldLines: 0,
-				NewLines: 3,
-			},
-			Output: &Fragment{
-				OldLines: 0,
-				NewLines: 3,
-				Lines: []FragmentLine{
-					{OpAdd, "new line 1\n"},
-					{OpAdd, "new line 2\n"},
-					{OpAdd, "new line 3\n"},
-				},
-				LinesAdded: 3,
-			},
-		},
-		"deleteAll": {
-			Input: `-old line 1
--old line 2
--old line 3
-`,
-			Fragment: Fragment{
-				OldLines: 3,
-				NewLines: 0,
-			},
-			Output: &Fragment{
-				OldLines: 3,
-				NewLines: 0,
-				Lines: []FragmentLine{
-					{OpDelete, "old line 1\n"},
-					{OpDelete, "old line 2\n"},
-					{OpDelete, "old line 3\n"},
-				},
-				LinesDeleted: 3,
-			},
-		},
-		"emptyContextLine": {
-			Input: ` context line
-
-+new line
- context line
-`,
-			Fragment: Fragment{
-				OldLines: 3,
-				NewLines: 4,
-			},
-			Output: &Fragment{
-				OldLines: 3,
-				NewLines: 4,
-				Lines: []FragmentLine{
-					{OpContext, "context line\n"},
-					{OpContext, "\n"},
-					{OpAdd, "new line\n"},
-					{OpContext, "context line\n"},
-				},
-				LinesAdded:      1,
-				LeadingContext:  2,
-				TrailingContext: 1,
-			},
-		},
-		"emptyChunk": {
-			Input: "",
-			Err:   true,
-		},
-		"invalidOperation": {
-			Input: ` context line
-?wat line
- context line
-`,
-			Fragment: Fragment{
-				OldLines: 3,
-				NewLines: 3,
-			},
-			Err: true,
-		},
-		"unbalancedHeader": {
-			Input: ` context line
--old line 1
-+new line 1
- context line
-`,
-			Fragment: Fragment{
-				OldLines: 2,
-				NewLines: 5,
-			},
-			Err: true,
-		},
-	}
-
-	for name, test := range tests {
-		t.Run(name, func(t *testing.T) {
-			p := newTestParser(test.Input, true)
-
-			frag := test.Fragment
-			err := p.ParseTextChunk(&frag)
-			if test.Err {
-				if err == nil {
-					t.Fatalf("expected error parsing text chunk, but got nil")
-				}
-				return
-			}
-			if err != nil {
-				t.Fatalf("error parsing text chunk: %v", err)
-			}
-
-			if !reflect.DeepEqual(test.Output, &frag) {
-				t.Errorf("incorrect fragment\nexpected: %+v\nactual: %+v", test.Output, &frag)
 			}
 		})
 	}
---
+++ gitdiff/parser_text_test.go
@@ -0,0 +1,315 @@
+package gitdiff
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParseTextFragmentHeader(t *testing.T) {
+	tests := map[string]struct {
+		Input  string
+		Output *Fragment
+		Err    bool
+	}{
+		"shortest": {
+			Input: "@@ -1 +1 @@\n",
+			Output: &Fragment{
+				OldPosition: 1,
+				OldLines:    1,
+				NewPosition: 1,
+				NewLines:    1,
+			},
+		},
+		"standard": {
+			Input: "@@ -21,5 +28,9 @@\n",
+			Output: &Fragment{
+				OldPosition: 21,
+				OldLines:    5,
+				NewPosition: 28,
+				NewLines:    9,
+			},
+		},
+		"trailingComment": {
+			Input: "@@ -21,5 +28,9 @@ func test(n int) {\n",
+			Output: &Fragment{
+				Comment:     "func test(n int) {",
+				OldPosition: 21,
+				OldLines:    5,
+				NewPosition: 28,
+				NewLines:    9,
+			},
+		},
+		"incomplete": {
+			Input: "@@ -12,3 +2\n",
+			Err:   true,
+		},
+		"badNumbers": {
+			Input: "@@ -1a,2b +3c,4d @@\n",
+			Err:   true,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			p := newTestParser(test.Input, true)
+
+			frag, err := p.ParseTextFragmentHeader()
+			if test.Err {
+				if err == nil {
+					t.Fatalf("expected error parsing header, but got nil")
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("error parsing header: %v", err)
+			}
+
+			if !reflect.DeepEqual(test.Output, frag) {
+				t.Errorf("incorrect fragment\nexpected: %+v\nactual: %+v", test.Output, frag)
+			}
+		})
+	}
+}
+
+func TestParseTextChunk(t *testing.T) {
+	tests := map[string]struct {
+		Input    string
+		Fragment Fragment
+
+		Output *Fragment
+		Err    bool
+	}{
+		"addWithContext": {
+			Input: ` context line
++new line 1
++new line 2
+ context line
+`,
+			Fragment: Fragment{
+				OldLines: 2,
+				NewLines: 4,
+			},
+			Output: &Fragment{
+				OldLines: 2,
+				NewLines: 4,
+				Lines: []FragmentLine{
+					{OpContext, "context line\n"},
+					{OpAdd, "new line 1\n"},
+					{OpAdd, "new line 2\n"},
+					{OpContext, "context line\n"},
+				},
+				LinesAdded:      2,
+				LeadingContext:  1,
+				TrailingContext: 1,
+			},
+		},
+		"deleteWithContext": {
+			Input: ` context line
+-old line 1
+-old line 2
+ context line
+`,
+			Fragment: Fragment{
+				OldLines: 4,
+				NewLines: 2,
+			},
+			Output: &Fragment{
+				OldLines: 4,
+				NewLines: 2,
+				Lines: []FragmentLine{
+					{OpContext, "context line\n"},
+					{OpDelete, "old line 1\n"},
+					{OpDelete, "old line 2\n"},
+					{OpContext, "context line\n"},
+				},
+				LinesDeleted:    2,
+				LeadingContext:  1,
+				TrailingContext: 1,
+			},
+		},
+		"replaceWithContext": {
+			Input: ` context line
+-old line 1
++new line 1
+ context line
+`,
+			Fragment: Fragment{
+				OldLines: 3,
+				NewLines: 3,
+			},
+			Output: &Fragment{
+				OldLines: 3,
+				NewLines: 3,
+				Lines: []FragmentLine{
+					{OpContext, "context line\n"},
+					{OpDelete, "old line 1\n"},
+					{OpAdd, "new line 1\n"},
+					{OpContext, "context line\n"},
+				},
+				LinesDeleted:    1,
+				LinesAdded:      1,
+				LeadingContext:  1,
+				TrailingContext: 1,
+			},
+		},
+		"deleteFinalNewline": {
+			Input: ` context line
+-old line 1
++new line 1
+\ No newline at end of file
+`,
+			Fragment: Fragment{
+				OldLines: 2,
+				NewLines: 2,
+			},
+			Output: &Fragment{
+				OldLines: 2,
+				NewLines: 2,
+				Lines: []FragmentLine{
+					{OpContext, "context line\n"},
+					{OpDelete, "old line 1\n"},
+					{OpAdd, "new line 1"},
+				},
+				LinesDeleted:   1,
+				LinesAdded:     1,
+				LeadingContext: 1,
+			},
+		},
+		"addFinalNewline": {
+			Input: ` context line
+-old line 1
+\ No newline at end of file
++new line 1
+`,
+			Fragment: Fragment{
+				OldLines: 2,
+				NewLines: 2,
+			},
+			Output: &Fragment{
+				OldLines: 2,
+				NewLines: 2,
+				Lines: []FragmentLine{
+					{OpContext, "context line\n"},
+					{OpDelete, "old line 1"},
+					{OpAdd, "new line 1\n"},
+				},
+				LinesDeleted:   1,
+				LinesAdded:     1,
+				LeadingContext: 1,
+			},
+		},
+		"addAll": {
+			Input: `+new line 1
++new line 2
++new line 3
+`,
+			Fragment: Fragment{
+				OldLines: 0,
+				NewLines: 3,
+			},
+			Output: &Fragment{
+				OldLines: 0,
+				NewLines: 3,
+				Lines: []FragmentLine{
+					{OpAdd, "new line 1\n"},
+					{OpAdd, "new line 2\n"},
+					{OpAdd, "new line 3\n"},
+				},
+				LinesAdded: 3,
+			},
+		},
+		"deleteAll": {
+			Input: `-old line 1
+-old line 2
+-old line 3
+`,
+			Fragment: Fragment{
+				OldLines: 3,
+				NewLines: 0,
+			},
+			Output: &Fragment{
+				OldLines: 3,
+				NewLines: 0,
+				Lines: []FragmentLine{
+					{OpDelete, "old line 1\n"},
+					{OpDelete, "old line 2\n"},
+					{OpDelete, "old line 3\n"},
+				},
+				LinesDeleted: 3,
+			},
+		},
+		"emptyContextLine": {
+			Input: ` context line
+
++new line
+ context line
+`,
+			Fragment: Fragment{
+				OldLines: 3,
+				NewLines: 4,
+			},
+			Output: &Fragment{
+				OldLines: 3,
+				NewLines: 4,
+				Lines: []FragmentLine{
+					{OpContext, "context line\n"},
+					{OpContext, "\n"},
+					{OpAdd, "new line\n"},
+					{OpContext, "context line\n"},
+				},
+				LinesAdded:      1,
+				LeadingContext:  2,
+				TrailingContext: 1,
+			},
+		},
+		"emptyChunk": {
+			Input: "",
+			Err:   true,
+		},
+		"invalidOperation": {
+			Input: ` context line
+?wat line
+ context line
+`,
+			Fragment: Fragment{
+				OldLines: 3,
+				NewLines: 3,
+			},
+			Err: true,
+		},
+		"unbalancedHeader": {
+			Input: ` context line
+-old line 1
++new line 1
+ context line
+`,
+			Fragment: Fragment{
+				OldLines: 2,
+				NewLines: 5,
+			},
+			Err: true,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			p := newTestParser(test.Input, true)
+
+			frag := test.Fragment
+			err := p.ParseTextChunk(&frag)
+			if test.Err {
+				if err == nil {
+					t.Fatalf("expected error parsing text chunk, but got nil")
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("error parsing text chunk: %v", err)
+			}
+
+			if !reflect.DeepEqual(test.Output, &frag) {
+				t.Errorf("incorrect fragment\nexpected: %+v\nactual: %+v", test.Output, &frag)
+			}
+		})
+	}
+}
