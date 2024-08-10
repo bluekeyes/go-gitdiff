9d11cc282ac3f3c960c934208535d64d0c8aabfa
Billy Keyes <commits@billykeyes.dev>
2024-08-09 21:00:28 -0700

    Add basic tests for patch formatting
    For now, assert that we produce exactly the same output as the original
    parsed input. I think these tests would ideally assert that the
    re-parsed value is the same, but it's kind of annoying to compare File
    objects without adding an external dependency.

---
+++ gitdiff/gitdiff_string_test.go
@@ -0,0 +1,45 @@
+package gitdiff
+
+import (
+	"bytes"
+	"os"
+	"testing"
+)
+
+func TestFile_String(t *testing.T) {
+	sources := []string{
+		"testdata/string/delete.patch",
+		"testdata/string/mode.patch",
+		"testdata/string/modify.patch",
+		"testdata/string/new.patch",
+		"testdata/string/new_empty.patch",
+		"testdata/string/new_mode.patch",
+		"testdata/string/rename.patch",
+		"testdata/string/rename_modify.patch",
+	}
+
+	for _, src := range sources {
+		b, err := os.ReadFile(src)
+		if err != nil {
+			t.Fatalf("failed to read %s: %v", src, err)
+		}
+
+		original := assertParseSingleFile(t, src, b)
+		str := original.String()
+
+		if string(b) != str {
+			t.Errorf("%s: incorrect patch\nexpected: %q\n  actual: %q\n", src, string(b), str)
+		}
+	}
+}
+
+func assertParseSingleFile(t *testing.T, src string, b []byte) *File {
+	files, _, err := Parse(bytes.NewReader(b))
+	if err != nil {
+		t.Fatalf("failed to parse patch %s: %v", src, err)
+	}
+	if len(files) != 1 {
+		t.Fatalf("expected %s to contain a single files, but found %d", src, len(files))
+	}
+	return files[0]
+}
